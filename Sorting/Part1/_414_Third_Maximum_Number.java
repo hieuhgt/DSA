0e483987fbb360ec62ffe1b621369e4b5a78477a
TrunHieu <trunghieu280999@gmail.com>
2023-08-27 23:34:24 +0700

    414 TODO

---
+++ Sorting/Part1/_414_Third_Maximum_Number.java
@@ -0,0 +1,14 @@
+package Sorting.Part1;
+
+public class _414_Third_Maximum_Number {
+  public static int thirdMax(int[] nums) {
+    // Insertion Sort [max to min]
+    // return a[2] ? a[2] : a[0]
+    return 1;
+  }
+
+  public static void main(String[] args) {
+    int[] a = { 2, 2, 3, 1 };
+    System.out.println(thirdMax(a));
+  }
+}
